c42c0f84ea4bea608a478a56ae868f402d234c24
RB <rbjocsing@up.edu.ph>
2024-03-06 22:36:25 +0800

    Finalized output for lab03

--- Lab03/blending.py
+++ Lab03/blending.py
@@ -1,8 +1,9 @@
 import cv2
 import numpy as np
-from scipy.signal import convolve2d as convolve
+from scipy.ndimage import convolve
 
-#   Define the binomial 5-tap filter
+#   Define a binomial 5-tap binomial filter from https://docs.opencv.org/3.4/d4/d1f/tutorial_pyramids.html
+#   According to the docs, this is the kernel that OpenCV pyrDown and pyrUp uses
 kernel = (1.0/256) * np.array([
                                 [1, 4, 6, 4, 1],
                                 [4, 16, 24, 16, 4],
@@ -25,11 +26,12 @@ def interpolate(img):
     processed_channels = [ ]
     
     for channel in channels:
+        #   Create a blank channel that is twice the area of the original image
         interpolated_channel = np.zeros((2 * channel.shape[0], 2 * channel.shape[1]))
-        #   Upsample each channel
+        #   Upsample each channel, by taking pixels from the original channel and using them to fill every other pixel of the blank channel
         interpolated_channel[::2, ::2] = channel
-        #   Blur and quadruple kernel area, since we interpolated by a factor of 2
-        interpolated_channel = convolve(interpolated_channel, 4 * kernel, mode='same')
+        #   Blur by quadrupling the kernel, since we interpolated by a factor of 2
+        interpolated_channel = convolve(interpolated_channel, 4 * kernel, mode='constant')
         processed_channels.append(interpolated_channel)
         
     interpolated_image = cv2.merge(processed_channels)
@@ -51,8 +53,8 @@ def decimate(img):
     processed_channels = [ ]
     
     for channel in channels:
-        #   Apply Gaussian blur then downsample each channel
-        decimated_channel = convolve(channel, kernel, mode='same')[::2, ::2]
+        #   Apply Gaussian blur then downsample each channel by taking every other pixel
+        decimated_channel = convolve(channel, kernel, mode='constant')[::2, ::2]
         processed_channels.append(decimated_channel)
         
     decimated_image = cv2.merge(processed_channels)
@@ -91,11 +93,11 @@ def construct_pyramids(image):
     laplacian_pyramid = []
     gaussian_pyramid = construct_gaussian_pyramid(image)
 
-    #   Subtract layers to max depth
+    #   Subtract layers to max depth, laplacian_i = gaussian_i - interpolate(gaussian_i+1)
     for i in range(len(gaussian_pyramid) - 1):
         laplacian_pyramid.append(gaussian_pyramid[i] - interpolate(gaussian_pyramid[i + 1]))
   
-    #   Exclude last level of the gaussian pyramid as it has no correspondent in the laplacian pyramid
+    #   Discard the last level of the gaussian pyramid as it has no correspondent in the laplacian pyramid
     return laplacian_pyramid, gaussian_pyramid[:-1]
 
 def reconstruct_image(pyramid):
@@ -129,14 +131,14 @@ def blend_pyramids(A, B, M):
         Returns:
                 blended_pyramid: a blended pyramid from two images and a mask.
     """
-    #   Retrieve Laplacian and Gaussian pyramids of images A and B, as well as the mask, assuming that they are all of the same dimensions
+    #   Retrieve Laplacian and Gaussian pyramids of images A and B, as well as the gaussian mask, assuming that they are all of the same dimensions
     LA , _ = construct_pyramids(A)
     LB , _ = construct_pyramids(B)
     _ , GM = construct_pyramids(M)
     
-    blended_pyramid = []
+    blended_pyramid = [ ]
     
-    #   Formula for blending pyramids that I lifted from https://github.com/twyunting/Laplacian-Pyramids.git and other sources
+    #   Formula for blending pyramids that I lifted from the blackboard (amazing!)
     for i in range(len(LA)):
         LS = GM[i] / 255 * LA[i] + (1 - GM[i] /255) * LB[i]
         blended_pyramid.append(bound(LS))
@@ -144,7 +146,6 @@ def blend_pyramids(A, B, M):
     return blended_pyramid
 
 def split_rgb(img):
-    
     """
         Wrapper around cv2.split() for splitting an image into individual channels.
         Parameters:
@@ -156,19 +157,21 @@ def split_rgb(img):
 
 def bound(img):
     """
-        Clip values between (0, 255) per pixel in the image.
+        Clip values between (0, 255) per pixel in the image. This is needed because there are instances where values are out of bounds.
+        Out of bound values in cv2 images generate noise and are unpleasant to look at.
         Parameters:
             img: the image to be normalized.
         Returns:
             clipped_image: the image with pixel values normalized to (0, 255) inclusive.
     """
-    img[img < 0] = 0
-    img[img > 255] = 255
-    return img.astype(np.uint8)
+    
+    #   Replace all pixel values less than 0 with 0, and values greater than 255 with 255, then return as an array of numpy integers
+    return np.where(img < 0, 0, np.where(img > 255, 255, img)).astype(np.uint8)
 
 def blend_image(img1, img2, mask):
     """
-    Blends two images together based on a mask.
+    Blends two images together based on a mask. All three images are assumed to be of the same size.
+    No check is performed to see if this assumption is met.
 
     Parameters:
         img1: The first image.
@@ -178,17 +181,9 @@ def blend_image(img1, img2, mask):
     Returns:
         blended_image: The blended image.
     """
-    #   Split images into RGB channels, assuming all three are of the same dimensions
-    img1R, img1G, img1B = split_rgb(img1)
-    img2R, img2G, img2B = split_rgb(img2)
-    mask, _, _ = split_rgb(mask)
     
-    #   Apply pyramid blending to each channel
-    R = reconstruct_image(blend_pyramids(img1R, img2R, mask))
-    G = reconstruct_image(blend_pyramids(img1G, img2G, mask))
-    B = reconstruct_image(blend_pyramids(img1B, img2B, mask))
-
-    #   Merge the blended channels back into an image
-    blended_image = cv2.merge((R, G, B))
-
-    return blended_image
+    assert img1.shape[0] == img2.shape[0] and img1.shape[1] == img2.shape[1], "Images are not of equal dimensions."
+    assert img1.shape[0] == mask.shape[0], "Mask does not match dimensions of the images."
+    
+    #   Chain the channel-agnostic functions above to generate a multi-resolution blended image? Not sure if that's what it's called.
+    return reconstruct_image(blend_pyramids(img1, img2, mask))
--- Lab03/lab03.py
+++ Lab03/lab03.py
@@ -1,13 +1,13 @@
 """
     Name: Computer Vision Laboratory 03
     Author: Rene Andre Bedonia Jocsing
-    Date Modified: 02/26/2024 
+    Date Modified: 03/06/2024 
     Usage: python lab03.py
     Description:
         This is a Python script that utilizes the cv2 package to implement an image blending
         algorithm using Laplacian and Gaussian pyramids.
-        All the necessary functions can be found in blend.py.
-        The testing suite can be found in lab03.py and should be used to access blend.py.
+        All the necessary functions can be found in blending.py.
+        The testing suite can be found in lab03.py and should be used to access blending.py.
 """
 
 import cv2
@@ -21,11 +21,13 @@ def main():
     #test_construct_gaussian_pyramids()     #   Passing
     #test_construct_pyramids()              #   Passing
     #test_blend_pyramids()                  #   Passing
-    test_reconstruct_image()               #   Passing
-    #test_blend_image()                     #   Passing
+    #test_reconstruct_image()               #   Passing
+    test_blend_image()                     #   Passing
     #test_blend_image_creative()            #   Passing
     return
-    
+
+"""UTILITIES"""
+
 #   Helper method to show an image
 def show_image(name, image):
     #   Show image in a normal window
@@ -34,7 +36,7 @@ def show_image(name, image):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
     
-#   Helper method for saving strips (key is used as a filename identifier)
+#   Helper method for saving an array-like of images (key is used as a filename identifier)
 def save_images(images, key):
     #   Save and/or display each strip
     for i, strip in enumerate(images):
@@ -44,11 +46,7 @@ def save_images(images, key):
 def save_image(image, key):
     cv2.imwrite(f'output/{key}.jpg', image)
 
-#   Helper method for naive brightening/dimming of image via multiplying each pixel by a brightness factor
-def brighten_image(image, brightness_factor):
-    return np.clip(image.astype(np.float32) * brightness_factor, 0, 255).astype(np.uint8)
-
-"""TESTING MODULES"""  
+"""TESTING SUITE"""  
 def test_interpolate():
     image = cv2.imread("input/image.png")
     
@@ -74,7 +72,7 @@ def test_decimate():
 def test_construct_gaussian_pyramids():
     image = cv2.imread("input/apple.jpg")
     
-    #print(f"Gaussian 0: {image.shape}")
+    print(f"Gaussian 0: {image.shape}")
     #show_image("Gaussian 0", image)
     
     gaussian_pyramid = construct_gaussian_pyramid(image)
@@ -86,30 +84,28 @@ def test_construct_gaussian_pyramids():
         show_image(f"Gaussian {i + 1}", bound(level))
         
 def test_construct_pyramids():
-    # Read the input image
     image = cv2.imread("input/apple.jpg")
 
-    #print(f"Laplacian 0: {image.shape}")
+    print(f"Laplacian 0: {image.shape}")
     show_image("Original", image)
     
-    # Construct the Laplacian pyramid
+    #   Construct the Laplacian pyramid
     laplacian_pyramid, gaussian_pyramid = construct_pyramids(image)
     
     #save_images(laplacian_pyramid, "laplacian")
     #save_images(gaussian_pyramid, "gaussian")
 
-    # Display the Gaussian pyramid
+    #   Display the Gaussian pyramid
     for i, level in enumerate(gaussian_pyramid):
         print(f"Gaussian {i + 1}: {level.shape}")
         show_image(f"Gaussian {i + 1}", bound(level))
     
-    # Display the Laplacian pyramid
+    #   Display the Laplacian pyramid
     for i, level in enumerate(laplacian_pyramid):
         print(f"Laplacian {i + 1}: {level.shape}")
         show_image(f"Laplacian {i + 1}", bound(level))
         
 def test_blend_pyramids():
-    # Read the input image
     A = cv2.imread("input/orange.jpg")
     B = cv2.imread("input/apple.jpg")
     M = cv2.imread("input/mask.jpg")
@@ -121,29 +117,28 @@ def test_blend_pyramids():
     
     #save_images(blended_pyramid, "blended")
 
-    # Display the Gaussian pyramid
+    # Display the blended pyramid
     for i, level in enumerate(blended_pyramid):
         print(f"Blended {i + 1}: {level.shape}")
-        show_image(f"Blended {i + 1}", level)
+        show_image(f"Blended {i + 1}", bound(level))
         
 def test_reconstruct_image():
-    # Read the input image
     image = cv2.imread("input/orange.jpg")
     
     show_image("Original Image", image)
     
     laplacian_pyramids, _ = construct_pyramids(image)
     
-    # for i, level in enumerate(laplacian_pyramids):
-    #     print(f"Laplacian {i + 1}: {level.shape}")
-    #     show_image(f"Laplacian {i + 1}", level)
+    for i, level in enumerate(laplacian_pyramids):
+         print(f"Laplacian {i + 1}: {level.shape}")
+         show_image(f"Laplacian {i + 1}", bound(level))
     
     reconstructed_image = reconstruct_image(laplacian_pyramids)
     
     show_image("Reconstructed Image", reconstructed_image)
     
 def test_blend_image():
-    # Read the images, A and B, as well as the mask M (we assume they are already of the same dimensions)
+    #   Read the images, A and B, as well as the mask M (we assume they are already of the same dimensions)
     A = cv2.imread("input/orange.jpg", cv2.IMREAD_COLOR)
     B = cv2.imread("input/apple.jpg", cv2.IMREAD_COLOR)
     M = cv2.imread("input/mask.jpg", cv2.IMREAD_COLOR)
@@ -152,18 +147,13 @@ def test_blend_image():
     show_image("Apple", B)
 
     blended_image = blend_image(A, B, M)
-    
-    #save_image(blended_image, "blended")
-
-    #   Brighten image for viewing purposes
-    #blended_image = brighten_image(blended_image, 1.5)
-    
+        
     #save_image(blended_image, "blended")
     
     show_image("Blended Image", blended_image)
     
 def test_blend_image_creative():
-    # Read the images, A and B, as well as the mask M (we assume they are already of the same dimensions)
+    #   Read the images, A and B, as well as the mask M (we assume they are already of the same dimensions)
     A = cv2.imread("input/pawn.png", cv2.IMREAD_COLOR)
     B = cv2.imread("input/bishop.png", cv2.IMREAD_COLOR)
     M = cv2.imread("input/crazymask.png", cv2.IMREAD_COLOR)
@@ -172,11 +162,6 @@ def test_blend_image_creative():
     show_image("Bishop", B)
 
     blended_image = blend_image(A, B, M)
-    
-    #save_image(blended_image, "crazy")
-
-    #   Brighten image for viewing purposes
-    #blended_image = brighten_image(blended_image, 1.5)
     
     #save_image(blended_image, "crazy")
     

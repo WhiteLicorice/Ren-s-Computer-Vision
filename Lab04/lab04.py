bfe3c9d9f7eb8babe8b0960681548ec3d218b094
RB <rbjocsing@up.edu.ph>
2024-03-08 20:23:41 +0800

    Collab start

--- Lab04/lab04.py
+++ Lab04/lab04.py
@@ -1,32 +1,55 @@
 """
     Name: Computer Vision Laboratory 04
-    Author: Rene Andre Bedonia Jocsing
-    Date Modified: 03/07/2024 
+    Author(s): Rene Andre Bedonia Jocsing & Ron Gerlan Naragdao
+    Date Modified: 03/08/2024 
     Usage: python lab04.py
     Description:
-        TODO: Place description here.
+        TODO: Place activity description here.
 """
 
 import cv2
 import numpy as np
 
+from thresholding import crop, canny_edge
+
 #   TODO: Find a way to dynamically threshold an image: https://docs.opencv.org/3.4/d7/d4d/tutorial_py_thresholding.html
 def main():
-    image = cv2.imread("input/absolute500ml.png", cv2.IMREAD_GRAYSCALE)
+    sample_images = {
+        '50ml': r'input/50mL/1.jpg',
+        '100ml': r'input/100ml/1.jpg',
+        '150ml': r'input/150ml/1.jpg',
+        '200ml': r'input/200ml/1.jpg',
+        '250ml': r'input/250ml/1.jpg',
+        '300ml': r'input/300ml/1.jpg',
+        '350ml': r'input/350ml/1.jpg',
+        'A': r'input/A/1.jpg',
+        'B': r'input/B/1.jpg',
+        'C': r'input/C/1.jpg',
+    }
     
-    show_image("Bottle", image)
-    
-    _, thresh = cv2.threshold(image, 150, 255, cv2.THRESH_BINARY)
-    
-    show_image("Thresholded", thresh)
-    
-    return
+    #   TODO: Calibrate numbers when detecting edges (this might be different for each ML category)
+    #   Preliminary investigation of images
+    for label, path in sample_images.items():
+        image = cv2.imread(path)
+        image = canny_edge(image)
+        #   Can pass parameters
+        #cropped_image = crop(image, x_lower=0, x_upper=2300, y_lower=250, y_upper=image.shape[0])
+        #   Defaults are provided if no parameters are passed
+        cropped_image = crop(image)
+        print(cropped_image.shape)
+        show_image(label, cropped_image)
+        #break
 
+    #   TODO: Find contours based on edges
+    #   TODO: Fit a bounding box around the contours
+    #   TODO: Find the area of the bounding box
+    
 #   Helper method to show an image
-def show_image(name, image):
+def show_image(image_label, image):
     #   Show image in a normal window
-    cv2.namedWindow(name, cv2.WINDOW_NORMAL)
-    cv2.imshow(name, image)
+    cv2.namedWindow(image_label, cv2.WINDOW_NORMAL)
+    cv2.resizeWindow(image_label, image.shape[1], image.shape[0])
+    cv2.imshow(image_label, image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
--- Lab04/thresholding.py
+++ Lab04/thresholding.py
@@ -1,3 +1,64 @@
-#   TODO: Implement some thresholding functions
+import cv2
+import numpy as np
 
-pass
+def crop(image: np.ndarray, 
+         x_lower: int = None, 
+         x_upper: int = None, 
+         y_lower: int = None, 
+         y_upper: int = None) -> np.ndarray:
+    
+    """
+        Crops an image along the x and y axis. The x and y values must
+        be within bounds of the image.
+
+        Parameters:
+                image: the original image
+                x_lower: the lower bound of x (default: 0)
+                x_upper: the upper bound of x (default: image.width)
+                y_lower: the lower bound of y (default: 0)
+                y_upper: the upper bound of y (default: image.height)
+                
+        Returns:
+                cropped_image: the image cropped according to the window formed by x_lower, x_upper, y_lower, and y_upper
+    """
+    
+    assert y_lower is None or y_lower >= 0 and y_upper is None or y_upper <= image.shape[0], "y out of bounds"
+    assert x_lower is None or x_lower >= 0 and x_upper is None or x_upper <= image.shape[1], "x out of bounds"
+    
+    #   Crop the image according to the parameters
+    cropped_image = image[y_lower or 0 : y_upper or image.shape[0], x_lower or 0: x_upper or image.shape[1]]
+    
+    return cropped_image
+
+#   TODO: Add other approaches for detecting the fluid threshold (eg. sobel edge detection, surf, etc.)
+#   TODO: Anything goes!
+
+def canny_edge(image: np.ndarray,
+               gaussian_kernel_size: tuple[int, int] = (5, 5),
+               canny_lower: int = 50,
+               canny_upper: int = 225):
+    """
+        Performs canny edge detection on an image. Automatically converts to greyscale
+        and applies gaussian blur to reduce noise.
+
+        Parameters:
+                image: the original image
+                gaussian_kernel_size: the size of the guassian kernel to be applied when blurring.
+                canny_lower: the lower threshold of the canny edge detection filter.
+                canny_upper: the upper threshold of the canny edge detection filter.
+                
+        Returns:
+                canny_edges: the high-pass filtered image with canny edges revealed.
+    """
+    assert gaussian_kernel_size[0] % 2 != 0 and gaussian_kernel_size[1] % 2 != 0, "Gaussian kernel dimensions must be odd."
+    
+    #   Convert the image to grayscale
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    #   Apply Gaussian blur to reduce noise
+    gray_image = cv2.GaussianBlur(gray_image, gaussian_kernel_size, 0)
+
+    #   Perform Canny edge detection
+    canny_edges = cv2.Canny(gray_image, canny_lower, canny_upper)
+
+    return canny_edges
